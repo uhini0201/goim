4da366918f6da927a685fa883aa65960dd5d99fd
uhinim <uhinim@trainee.nrifintech.com>
2023-05-07 21:55:16 +0530

    updated graphs

--- graph_plot.py
+++ graph_plot.py
@@ -8,7 +8,7 @@ y1 = []
 x2 = []
 y2 = []
 
-file1 = open('output/output_graph/twitter_combined_WC_maxdegree_15_50_1487723611282_1682961489163.txt', 'r')
+file1 = open('output/output_graph/twitter_combined_WC_discountdegree_15_50_1487723611282_1682961523703.txt', 'r')
 Lines = file1.readlines()
 
 count = 0
@@ -19,6 +19,7 @@ for line in Lines:
     lines = [i for i in line.split(", ")]
     if len(lines) == 1:
         continue
+    
 
     x2.append(count)
     y2.append(lines[0])
@@ -29,8 +30,11 @@ for line in Lines:
     x1.append(x)
     y1.append(y)
 
+y1.sort()
 plt.scatter(x1, y1)
 plt.show()
 
+
+y2.sort()
 plt.plot(x2, y2)
 plt.show()
